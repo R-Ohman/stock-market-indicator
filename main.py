d7c9a2928d1131555d877ad9596a8c000698d0da
Ruslan Rabadanov <vonadabarr@gmail.com>
2024-03-08 21:27:44 +0100

    data processing and s&p500 chart

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import csv
+import matplotlib.pyplot as plt
+from datetime import datetime
 
 
-# Press the green button in the gutter to run the script.
+def process_data():
+    with open('data/SnP500.csv', 'r') as file:
+        reader = csv.DictReader(file)
+        data = []
+        for row in reader:
+            date = datetime.strptime(row['Date'], '%Y-%m-%d')
+            value = float(row['Closing'])
+            data.append((date, value))
+
+        # Sort the data by date
+        data.sort(key=lambda x: x[0])
+    return data
+
+
+def create_plot(x, y, x_label, y_label, title):
+    plt.xlabel(x_label)
+    plt.ylabel(y_label)
+    plt.title(title)
+    plt.plot(x, y)
+    plt.show()
+
+
+def main():
+    dates, values = zip(*process_data())
+    create_plot(dates, values, 'Date', 'S&P 500', 'S&P 500 Closing Prices')
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
